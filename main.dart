9bba2e3b751d24fd749c5012a5fab932bc50278c
Gohul13 <154820398+Gohul13@users.noreply.github.com>
2025-07-25 11:51:20 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'services/auth_service.dart';
+import 'services/task_service.dart';
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthService()),
+        ChangeNotifierProvider(create: (_) => TaskService()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Consumer<AuthService>(
+          builder: (_, auth, __) => auth.user == null ? LoginScreen() : HomeScreen(),
+        ),
+      ),
+    );
+  }
+}
